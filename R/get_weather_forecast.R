32d3d01a477592ff27b2484cc6a626a67e41a496
paulnest <paul.nestour7@gmail.com>
2024-05-23 19:19:18 +0200

    MAJ

--- R/get_weather_forecast.R
+++ R/get_weather_forecast.R
@@ -19,11 +19,11 @@
 #' @importFrom tibble as_tibble
 #'
 #' @export
-perform_request <- function(lat, lon) {
+perform_request <- function(lat, long) {
   api_url <- "https://api.open-meteo.com/v1/forecast"
   params <- list(
     latitude = lat,
-    longitude = lon,
+    longitude = long,
     hourly = c(
       "temperature_2m",
       "apparent_temperature", "precipitation_probability",
@@ -32,10 +32,15 @@ perform_request <- function(lat, lon) {
   )
   req <- request(api_url) |>
     req_url_query(!!!params, .multi = "comma") |>
+    # Exécution de la requête et récupération des résultats
     req_perform() |>
-    resp_body_json()
+    # sauf qu'ils sont au format json (Content-Type: application/json)
+    # donc on a besoin de cette étape :
+    # Extraction des données JSON de la réponse
+    resp_body_json() |>
+    as_tibble()
   
-  as_tibble(req)
+  return(req)
 }
 
 #' unnest_response
@@ -72,11 +77,11 @@ perform_request <- function(lat, lon) {
 #' @export
 unnest_response <- function(resp) {
   tibble(
-    date_heure = unlist(resp$time),
-    temperature_celsius = unlist(resp$temperature_2m),
-    temperature_ressentie_celsius = unlist(resp$apparent_temperature),
-    precipation_proba = unlist(resp$precipitation_probability),
-    precipitation = unlist(resp$precipitation)
+    date_heure = unlist(resp$hourly[1][[1]]),
+    temperature_celsius = unlist(resp$hourly[2][[1]]),
+    temperature_ressentie_celsius = unlist(resp$hourly[3][[1]]),
+    precipation_proba = unlist(resp$hourly[4][[1]]),
+    precipitation = unlist(resp$hourly[5][[1]])
   )
 }
 
@@ -95,8 +100,7 @@ unnest_response <- function(resp) {
 #' @export
 address_to_gps <- function(adresse, lat, long) {
   lieu <- tibble(
-    adresse
-  ) |>
+    adresse) |>
     geocode(address = adresse, method = "osm") |>
     dplyr::select(lat, long)
   
